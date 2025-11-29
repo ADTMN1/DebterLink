8004f4326e8617fd5a6e0a06a58b7db5a562366f
SifanGeremu <wins33975@gmail.com>
2025-11-29 20:51:51 +0300

    feat- registration finalized

--- backend/src/controllers/register.controller.js
+++ backend/src/controllers/register.controller.js
@@ -1,31 +1,62 @@
 const db = require("../../config/db.config");
-const {hashPassword,comparePassword} = require("../Utils/hash");
+const { hashPassword, comparePassword } = require("../Utils/hash");
 
 // function to handle user registration
-const registerUser = async(req,res)=>{
-   
-    try{ const {username,email,password}=req.body;
-// check if user already exists
-const existingUser=await db.query("SELECT * FROM users WHERE email = $1 [email]");
-if(existingUser.rows.length>0){
-    return res.status(400).json({message:"User already exists"});
-}
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-const hashedpassword= await hashPassword(password);
+    // Basic constraints for password validation
+    const confirmPassword = req.body.confirmPassword;
+    if (!password) {
+      return res.status(400).json({ error: "Password is required" });
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 8 characters long" });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match" });
+    }
 
-// insert new user into database
-const newUser=await db.query("INSERT INTO user (username,email,password) VALUES ($1,$2,$3) RETURNING *",[username,email,hashedpassword]);
+    // Regex for complexity
+    const strongRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])/;
+    if (!strongRegex.test(password)) {
+      return res.status(400).json({
+        error:
+          "Password must include uppercase, lowercase, number, and special character",
+      });
+    }
+    // check if user already exists
+    const existingUser = await db.query(
+      "SELECT * FROM users WHERE email = $1 [email]"
+    );
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-const user = newUser.rows[0];
+    const hashedpassword = await hashPassword(password);
 
-const AccessToken = generateAccessToken(user);
-const RefreshToken = generateRefreshToken(user);
+    // insert new user into database
+    const newUser = await db.query(
+      "INSERT INTO user (username,email,password) VALUES ($1,$2,$3) RETURNING *",
+      [username, email, hashedpassword]
+    );
 
+    const user = newUser.rows[0];
 
-res.status(201).json({message:"user registered successfully",AccessToken,RefreshToken});}
-catch(error){
-    console.error("Error registering user:",error);
-    res.status(500).json({message:"something went wrong try again later"});
-}
-}
-module.exports={registerUser};
+    const AccessToken = generateAccessToken(user);
+    const RefreshToken = generateRefreshToken(user);
+
+    res.status(201).json({
+      message: "user registered successfully",
+      AccessToken,
+      RefreshToken,
+    });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "something went wrong try again later" });
+  }
+};
+module.exports = { registerUser };
