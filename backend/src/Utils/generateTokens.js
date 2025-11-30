2734c254ed641f4de8826b4b1740306026d62c6c
SifanGeremu <wins33975@gmail.com>
2025-11-30 19:29:06 +0300

    separated query from controllers

--- backend/src/Utils/generateTokens.js
+++ backend/src/Utils/generateTokens.js
@@ -1,14 +1,17 @@
-const JWT = require('jsonwebtoken');
+import jwt from "jsonwebtoken";
 
-const generateAccessToken =(user)=>{
-    // implementation for generating access token
+export const generateAccessToken = (user) => {
+  return jwt.sign(
+    { id: user.id, email: user.email, full_name: user.full_name },
+    process.env.JWT_ACCESS_SECRET, 
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY || "15m" }
+  );
+};
 
-    return jwt.sign({id:user.id,email:user.email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'15m'});
-}
-
-const generateRefreshToken= (user)=>{
-    // implementation for generating refresh token
-    return jwt.sign({id:user.id,email:user.email},process.env.REFRESH_TOKEN_SECRET,{expiresIn:'7d'});
-}
-
-module.exports={generateAccessToken,generateRefreshToken};  
+export const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { id: user.id, email: user.email, full_name: user.full_name },
+    process.env.JWT_REFRESH_SECRET, 
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY || "7d" }
+  );
+};
