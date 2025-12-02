3181803a6d29b193409d28656a1509989f0d29e0
Melkias Teshoma <melkiasapo@gmail.com>
2025-12-02 09:47:07 +0300

    basic six routes defined auth and role based route configuration is finished

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use("/auth", authRoutes);
 
 
-router.get("/super-admin",    authMiddleware,  verifyRole(["SUPER_ADMIN"]) , (req, res) => res.send("SUPER ADMIN route"));
+router.get("/super-admin",    authMiddleware,  verifyRole() , (req, res) => res.send("SUPER ADMIN route"));
 router.get("/director",  authMiddleware,  verifyRole(1) , (req, res) => res.send("DIRECTOR route"));
 router.get("/admin",  authMiddleware,  verifyRole(5) , (req, res) => res.send("admin route"));
 router.get("/parent",  authMiddleware,  verifyRole(3) , (req, res) => res.send("parent route"));
