cddded0ad692c0470161cab88da6e0dcee003ab0
Melkias Teshoma <melkiasapo@gmail.com>
2025-12-10 21:25:42 +0300

    Delete web_client

--- web_client/client/src/services/index.ts
+++
@@ -1,7 +0,0 @@
-// Export all services
-export * from './api';
-export * from './authService';
-export * from './gradesService';
-export * from './attendanceService';
-export * from './behaviorService';
-export * from './resultsService';
